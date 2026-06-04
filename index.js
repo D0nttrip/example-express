7d7611a71a7ca2ba07bd81e4f86b80da0da876f9
D0nttrip <D0ntTriptryingtocatchme003@proton.me>
2026-06-04 06:22:48 +0000

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ app.get("/", (req, res) => {
   console.log("----------------------");
 
   res.send(`
-    <h1>YOUR IP is mine now</h1>
+    <h1>Your IP is mine now</h1>
+    <iframe src="https://giphy.com/embed/3xQmLh3c47k0tZuKc4" width="480" height="370" style="" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/laugh-to-sad-omegle-wtf-omegletv-3xQmLh3c47k0tZuKc4">via GIPHY</a></p>
     <iframe src="https://giphy.com/embed/W2BaHNYfOBGuKHjlZO" width="480" height="480" style="" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/TruRebelsDistro-69-6ix9ine-sixnine-W2BaHNYfOBGuKHjlZO">via GIPHY</a></p>
     <p>Your visit has been recorded.</p>
   `);
